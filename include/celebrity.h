3c85c17528fac1d48adaa81f11d7405e13f2bd6f
Runquan Ye <yer@eos24.cis.gvsu.edu>
2018-04-03 00:01:06 -0400

    add the comments to the methods

--- include/celebrity.h
+++ include/celebrity.h
@@ -10,48 +10,68 @@
 class celebrity {
 
 	public:
-		/**
-	 	*Constructor to initialize a celebrity
-	 	*/
+		/**************************************
+	 	 *Constructor to initialize a celebrity
+	 	 *************************************/
 		celebrity(std::string name, std::string famous_field, int desire, bool haveMet);
 
-		/**
- 		*Empty celebrity constructor
- 		*/ 	
+		/**************************************
+ 		 *Empty celebrity constructor
+ 		 *************************************/ 	
 		celebrity();
 
-		/**
+		/*************************************
 		 *Returns the name of the celebrity
 		 *@return string - celebrity's name
-		 */
+		 *************************************/
 		std::string getName() const;
 
-		/*&
- 		*Returns the area in which the celebrity is famous in 
-		@return string - field in which the celeb is famous in 
- 		*/	 	
+		/********************************************************
+ 		 *Returns the area in which the celebrity is famous in 
+		 *@return string - field in which the celeb is famous in 
+ 		 *******************************************************/	 	
 		std::string getArea() const;
 
-		/**
- 		*Returns how badly we want to meet the celebrity
-		*@return int - how badly we want to meet 0-10 
- 		*/ 	
+		/*******************************************************
+ 		 *Returns how badly we want to meet the celebrity
+		 *@return int - how badly we want to meet 0-10 
+ 		 ******************************************************/ 	
 		int getDesire() const;
 
-		/**
- 		*Whether or not we have met the celeb
-		*@return bool - have met or not
- 		*/	
+		/******************************************************
+ 		 *Whether or not we have met the celeb
+		 *@return bool - have met or not
+ 		 *****************************************************/	
 		bool hasMet() const;
 
+		/**************************************
+ 		 *set the celebrity's name
+		 *@param string, name
+ 		 *************************************/ 
 		void setName(const std::string name);
 
+		/***************************************************
+ 		 *set the desire that you want to see this celebrity
+		 *@param int, desireVal
+ 		 **************************************************/ 
 		void setDesire(const int desireVal);
 
+		/*********************************************************
+ 		 *set a boolean to check did you meet with this celebrity
+		 *@param boolean, met
+ 		 ********************************************************/
 		void setMet(bool met);
 
+		/****************************************************************
+ 		 *set a boolean to check the relationship between the celebrities
+		 *@param celebrity, c1, c2
+ 		 ***************************************************************/
 		friend bool operator<(const celebrity c1, const celebrity c2);
 
+		/****************************************************************
+ 		 *set a boolean to check the relationship between the celebrities
+		 *@param celebrity, c1, c2
+ 		 ***************************************************************/
 		friend bool operator<=(const celebrity c1, const celebrity c2);		
 
 	private:
@@ -69,7 +89,5 @@ class celebrity {
 
 };
 
-//		bool operator<(const celebrity c1, const celebrity c2);
 
-//		bool operator<=(const celebrity c1, const celebrity c2);
 #endif
--- src/celebrity.cpp
+++ src/celebrity.cpp
@@ -1,55 +1,87 @@
 #include "celebrity.h"
 #include <string>
 
-/** 
+/************************************************************ 
  * This class represents an interface for our celebrity class
  * @author Runquan Ye, Nabeel Vali
- */
+ ***********************************************************/
 
-	/**
- 	*Default empty constructor for initializing a celebrity
- 	*/ 	
+	/*******************************************************
+ 	 *Default empty constructor for initializing a celebrity
+ 	 ******************************************************/ 	
 	celebrity::celebrity(){}
 
-	/**
+	/*******************************************************
 	 * Constructor to initialize a celebrity with parameters
-	 */
+	 ******************************************************/
 	celebrity::celebrity(std::string name, std::string famous_field, int desire, bool haveMet){
 				
-				  this -> name = name;
-				  this -> famous_field = famous_field;
-				  this -> desire = desire;
-				  this -> haveMet = haveMet;
+		this -> name = name;
+		this -> famous_field = famous_field;
+		this -> desire = desire;
+		this -> haveMet = haveMet;
 	}
 
+	/*************************************
+	 *Returns the name of the celebrity
+	 *@return string - celebrity's name
+	 *************************************/
 	std::string celebrity::getName() const{
 		return name;
 	}
 
+	/********************************************************
+ 	 *Returns the area in which the celebrity is famous in 
+	 *@return string - field in which the celeb is famous in 
+ 	 *******************************************************/
 	std::string celebrity::getArea() const{
 		return famous_field;
 	}
 
+	/*******************************************************
+ 	 *Returns how badly we want to meet the celebrity
+	 *@return int - how badly we want to meet 0-10 
+ 	 ******************************************************/ 
 	int celebrity::getDesire() const{
 		return desire;
 	}
 
+	/******************************************************
+ 	 *Whether or not we have met the celeb
+	 *@return bool - have met or not
+ 	 *****************************************************/
 	bool celebrity::hasMet() const{
 		return haveMet;
 	}
 
+	/******************************************************
+ 	 *Whether or not we have met the celeb
+	 *@return bool - have met or not
+ 	 *****************************************************/	
 	void celebrity::setName(const std::string name){
 		this -> name = name;
 	}
 
+	/*******************************************************
+ 	 *Returns how badly we want to meet the celebrity
+	 *@return int - how badly we want to meet 0-10 
+ 	 ******************************************************/ 
 	void celebrity::setDesire(const int desireVal){
 		this -> desire = desireVal;
 	}
 
+	/*********************************************************
+ 	 *set a boolean to check did you meet with this celebrity
+	 *@param boolean, met
+ 	 ********************************************************/
 	void celebrity::setMet(bool met){
 		this -> haveMet = met;
 	}
 
+	/****************************************************************
+ 	 *set a boolean to check the relationship between the celebrities
+	 *@param celebrity, c1, c2
+ 	 ***************************************************************/
 	bool operator<(const celebrity c1, const celebrity c2){
 		if(c1.getDesire() > c2.getDesire()){
 			return false;
@@ -61,6 +93,10 @@
 			return false;
 	}
 
+	/****************************************************************
+ 	 *set a boolean to check the relationship between the celebrities
+	 *@param celebrity, c1, c2
+ 	 ***************************************************************/
 	bool operator<=(const celebrity c1, const celebrity c2){
 		return c1 < c2;
 		
